c1cad73f17f062ad3c4ff79faf4be7a6f44666c4
Jon Lu <lujonghao20@gmail.com>
2019-04-24 00:35:41 -0400

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Jon Lu on 4/24/19.
+//  Copyright © 2019 Jon Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
